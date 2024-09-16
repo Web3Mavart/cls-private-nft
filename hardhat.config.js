6355b1cc095200cf8acc76123d88fad1d1c3060d
Web3Mavart <155269092+Web3Mavart@users.noreply.github.com>
2024-09-16 15:03:26 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x60fccc19c4f1a402459afeec6e15035d734db6fffb4956b0c597ec6a6c357a68`],
+      accounts: [``],
     },
   },
 };
